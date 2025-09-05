a9c92524aa87e89691dfb79900949ab8225df2ca
Jack Kirk (RIT Student) <jwk5651@rit.edu>
2025-09-05 14:46:24 -0400

    Push for GitHub Actions

--- src/imageResponses.js
+++ src/imageResponses.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 const sponge = fs.readFileSync(`${__dirname}/../client/spongegar.png`);
 
+// writes spongegar image to page
 const getSponge = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'image/png' });
   response.write(sponge);
